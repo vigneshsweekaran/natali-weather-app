6f1f8475e5085009c86ab4e122360486d8f88db8
vigneshsweekaran <vigneshsweekaran@gmail.com>
2021-08-10 12:36:28 +0530

    updated

--- app.py
+++ app.py
@@ -9,15 +9,15 @@ color = "#16a085"
 @app.route("/")
 def hello():
 
-    url = "https://weatherapi-com.p.rapidapi.com/search.json"
-    querystring = {"q":"Paris"}
+    url = "https://weatherbit-v1-mashape.p.rapidapi.com/current"
+    querystring = {"lon":"38.5","lat":"-78.5"}
     headers = {
         'x-rapidapi-key': "cd1c88eda0msh32b10551c600641p14a147jsnc57be3d23020",
-        'x-rapidapi-host': "weatherapi-com.p.rapidapi.com"
+        'x-rapidapi-host': "weatherbit-v1-mashape.p.rapidapi.com"
     }
     response = requests.request("GET", url, headers=headers, params=querystring)
     print(response.text)
-    return render_template('index.html', contents=response, color=color)
+    return render_template('index.html', contents=response.text, color=color)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0",port=5000,debug=True,use_reloader=True)
